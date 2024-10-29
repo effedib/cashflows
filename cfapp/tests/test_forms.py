3e8080fad5d655f1a53958d993ce29f1b09b1955
Francesco Di Bella <f.dibella@fidesct.onmicrosoft.com>
2024-10-29 16:57:58 +0100

    ruff format test_forms

--- cfapp/tests/test_forms.py
+++ cfapp/tests/test_forms.py
@@ -3,25 +3,42 @@ from django.test import TestCase
 from ..models import Committenti, Canali, TipologiaTransazione, Transazione, Incasso
 from ..forms import TransazioneForm, IncassoForm
 
+
 class TestTransazioneForm(TestCase):
     def setUp(self):
-        self.tipologia_transazione1 = TipologiaTransazione.objects.create(tipologia_transazione="Transazione 1")
-        self.tipologia_transazione2 = TipologiaTransazione.objects.create(tipologia_transazione="Transazione 2")
+        self.tipologia_transazione1 = TipologiaTransazione.objects.create(
+            tipologia_transazione="Transazione 1"
+        )
+        self.tipologia_transazione2 = TipologiaTransazione.objects.create(
+            tipologia_transazione="Transazione 2"
+        )
         self.transazione1 = Transazione.objects.create(
             importo=50, tipologia=self.tipologia_transazione1, data="2023-01-15"
         )
         self.transazione2 = Transazione.objects.create(
             importo=75, tipologia=self.tipologia_transazione2, data="2023-02-15"
         )
-        self.committente1 = Committenti.objects.create(codice="ABC", committente="Committente 1")
-        self.committente2 = Committenti.objects.create(codice="DEF", committente="Committente 2")
+        self.committente1 = Committenti.objects.create(
+            codice="ABC", committente="Committente 1"
+        )
+        self.committente2 = Committenti.objects.create(
+            codice="DEF", committente="Committente 2"
+        )
         self.canale1 = Canali.objects.create(canale="Canale 1", flag_auto_versato=False)
         self.canale2 = Canali.objects.create(canale="Canale 2", flag_auto_versato=True)
         self.incasso1 = Incasso.objects.create(
-            importo=100, data="2023-01-01", ricevuta="123456", canale=self.canale1, committente=self.committente1
+            importo=100,
+            data="2023-01-01",
+            ricevuta="123456",
+            canale=self.canale1,
+            committente=self.committente1,
         )
         self.incasso2 = Incasso.objects.create(
-            importo=200, data="2023-02-01", ricevuta="789012", canale=self.canale2, committente=self.committente2
+            importo=200,
+            data="2023-02-01",
+            ricevuta="789012",
+            canale=self.canale2,
+            committente=self.committente2,
         )
 
     def test_transazione_form_valid(self):
@@ -29,7 +46,7 @@ class TestTransazioneForm(TestCase):
             "importo": 80,
             "tipologia": self.tipologia_transazione1.id,
             "data": "2023-03-01",
-            "incassi": [self.incasso1.id, self.incasso2.id]
+            "incassi": [self.incasso1.id, self.incasso2.id],
         }
         form = TransazioneForm(data=data)
         self.assertTrue(form.is_valid())
@@ -45,24 +62,33 @@ class TestTransazioneForm(TestCase):
             "importo": -50,
             "tipologia": self.tipologia_transazione1.id,
             "data": "2023-03-41",
-            "incassi": [self.incasso1.id, self.incasso2.id]
+            "incassi": [self.incasso1.id, self.incasso2.id],
         }
         form = TransazioneForm(data=data)
         self.assertFalse(form.is_valid())
         self.assertIn("data", form.errors)
 
+
 class TestIncassoForm(TestCase):
     def setUp(self):
-        self.tipologia_transazione1 = TipologiaTransazione.objects.create(tipologia_transazione="Transazione 1")
-        self.tipologia_transazione2 = TipologiaTransazione.objects.create(tipologia_transazione="Transazione 2")
+        self.tipologia_transazione1 = TipologiaTransazione.objects.create(
+            tipologia_transazione="Transazione 1"
+        )
+        self.tipologia_transazione2 = TipologiaTransazione.objects.create(
+            tipologia_transazione="Transazione 2"
+        )
         self.transazione1 = Transazione.objects.create(
             importo=50, tipologia=self.tipologia_transazione1, data="2023-01-15"
         )
         self.transazione2 = Transazione.objects.create(
             importo=75, tipologia=self.tipologia_transazione2, data="2023-02-15"
         )
-        self.committente1 = Committenti.objects.create(codice="ABC", committente="Committente 1")
-        self.committente2 = Committenti.objects.create(codice="DEF", committente="Committente 2")
+        self.committente1 = Committenti.objects.create(
+            codice="ABC", committente="Committente 1"
+        )
+        self.committente2 = Committenti.objects.create(
+            codice="DEF", committente="Committente 2"
+        )
         self.canale1 = Canali.objects.create(canale="Canale 1", flag_auto_versato=False)
         self.canale2 = Canali.objects.create(canale="Canale 2", flag_auto_versato=True)
 
@@ -74,7 +100,7 @@ class TestIncassoForm(TestCase):
             "canale": self.canale1.id,
             "committente": self.committente1.id,
             "versato": True,
-            "transazioni": [self.transazione1.id, self.transazione2.id]
+            "transazioni": [self.transazione1.id, self.transazione2.id],
         }
         form = IncassoForm(data=data)
         self.assertTrue(form.is_valid())
@@ -86,7 +112,9 @@ class TestIncassoForm(TestCase):
         self.assertEqual(incasso.canale, self.canale1)
         self.assertEqual(incasso.committente, self.committente1)
         self.assertTrue(incasso.versato)
-        self.assertCountEqual(incasso.transazioni.all(), [self.transazione1, self.transazione2])
+        self.assertCountEqual(
+            incasso.transazioni.all(), [self.transazione1, self.transazione2]
+        )
 
     def test_incasso_form_invalid(self):
         data = {
@@ -96,7 +124,7 @@ class TestIncassoForm(TestCase):
             "canale": self.canale1.id,
             "committente": self.committente1.id,
             "versato": True,
-            "transazioni": [self.transazione1.id, self.transazione2.id]
+            "transazioni": [self.transazione1.id, self.transazione2.id],
         }
         form = IncassoForm(data=data)
         self.assertFalse(form.is_valid())
