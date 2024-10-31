48b1a76b217156fa007657342c50ab9f15476669
Francesco Di Bella <f.dibella@fidesct.onmicrosoft.com>
2024-10-31 09:44:07 +0100

    signals work but wrong updates

--- cfapp/apps.py
+++ cfapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CfappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cfapp"
+
+    def ready(self):
+        import cfapp.signals
